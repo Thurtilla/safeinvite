f3ac33ff3e83400f8bbddf2c865a1b3d7b11ac38
Thurtilla <kjetil@svergja.com>
2021-11-30 00:31:41 +0100

    cleaned up code

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,11 @@
-Testing automatic upload to curseforge
+v0.4
+Cleaned up code
+
+v0.3
+Issues with host
+
+v0.2
+small fix
+
+V0.1
+Basic tests complete
--- main.lua
+++ main.lua
@@ -15,14 +15,12 @@ SLASH_SAFEINVITE1, SLASH_SAFEINVITE2 = "/safeinvite", "/sinv"
 SlashCmdList["SAFEINVITE"] = function(msg)
 	if msg == "on" then
 		print(safeInviteName .. "Addon will now hide 'Invite' from context menu's. Use /sinv off to show again.")
-		safeinvite_status = true
-		toggleInviteFromChatContext()
-		toggleInviteFromTargetContext()
+		safeinvite_enabled = true
+		toggleOnContext()
 	elseif msg == "off" then
 		print(safeInviteName .. "Addon is now disabled, to turn on, use /sinv on")
-		safeinvite_status = false
-		toggleInviteFromChatContext()
-		toggleInviteFromTargetContext()
+		safeinvite_enabled = false
+		toggleOnContext()
 	else
 		print(safeInviteName .. "Possible commands is:")
 		print(safeInviteName .. "/sinv on - turns on removal of Invite from contexts")
@@ -34,40 +32,41 @@ function myframe:ADDON_LOADED(addonName)
 	if addonName ~= "safeinvite" then
 		return
 	end
-	if safeinvite_status == nil then
+	if safeinvite_enabled == nil then
 		print(safeInviteName .. "First load, enabling addon")
-		safeinvite_status = true
+		safeinvite_enabled = true
 	end
-	if safeinvite_status == true then
-		toggleInviteFromChatContext()
-		toggleInviteFromTargetContext()
-	end
-end
-
-function toggleInviteFromChatContext()
-	if safeinvite_status then
-		for i = 1, #UnitPopupMenus["FRIEND"] do
-			if UnitPopupMenus["FRIEND"][i] == "INVITE" then
-				safeinvite_inviteindex_FRIEND = i
-				inviteTableFriend = UnitPopupMenus["FRIEND"][i]
-				tremove(UnitPopupMenus["FRIEND"], i)
-			end
-		end
-	else
-		tinsert(UnitPopupMenus["FRIEND"], safeinvite_inviteindex_FRIEND, inviteTableFriend)
+	if safeinvite_enabled == true then
+		toggleOnContext()
 	end
 end
 
-function toggleInviteFromTargetContext()
-	if safeinvite_status then
-		for i = 1, #UnitPopupMenus["PLAYER"] do
-			if UnitPopupMenus["PLAYER"][i] == "INVITE" then
-				inviteTablePlayer = UnitPopupMenus["PLAYER"][i]
-				safeinvite_inviteindex_PLAYER = i
-				tremove(UnitPopupMenus["PLAYER"], i)
+function toggleOnContext()
+	if safeinvite_enabled then
+		for k, v in pairs({"PLAYER", "FRIEND"}) do
+			local popupMenu = UnitPopupMenus[v]
+			for i = 1, #popupMenu do
+				if popupMenu[i] == "INVITE" then
+					if v == "PLAYER" then
+						safeinvite_table_PLAYER = popupMenu[i]
+						safeinvite_inviteindex_PLAYER = i
+					elseif v == "FRIEND" then
+						safeinvite_table_FRIEND = popupMenu[i]
+						safeinvite_inviteindex_FRIEND = i
+					end
+					tremove(popupMenu, i)
+					break
+				end
 			end
 		end
 	else
-		tinsert(UnitPopupMenus["PLAYER"], safeinvite_inviteindex_PLAYER, inviteTablePlayer)
+		for k, v in pairs({"PLAYER", "FRIEND"}) do
+			local popupMenu = UnitPopupMenus[v]
+			if v == "PLAYER" and popupMenu[safeinvite_inviteindex_PLAYER] ~= "INVITE" then
+				tinsert(popupMenu, safeinvite_inviteindex_PLAYER, safeinvite_table_PLAYER)
+			elseif v == "FRIEND" and popupMenu[safeinvite_inviteindex_FRIEND] ~= "INVITE" then
+				tinsert(popupMenu, safeinvite_inviteindex_FRIEND, safeinvite_table_FRIEND)
+			end
+		end
 	end
 end
